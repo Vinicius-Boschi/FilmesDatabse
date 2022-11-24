f919affd1a94c9d1703cdcf13e17651981dffba8
Vinicius Boschi <vinicius.yg@gmail.com>
2022-11-24 15:49:12 -0300

    Finalizando as infos iniciais

--- pages/details/[id].jsx
+++ pages/details/[id].jsx
@@ -18,7 +18,7 @@ export default function Post({ movie }) {
               <div className="image">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
               </div>
-              <div>
+              <div className="posters-infos">
                 <section>
                   <div className="titles">
                     <h1 className="title">{movie.title}</h1>
@@ -27,13 +27,29 @@ export default function Post({ movie }) {
                         <div className="facts-infos">
                         {movie.genres.map((genre, index) => {
                           return (
-                            <p key={index}>
-                              <span>{genre.name}</span>
-                            </p>
+                            <span key={index}>{genre.name}</span>
                           )
                         })}
+                         <span className="hour">{result}</span>
                         </div>
-                        <p>{result}</p>
+                    </div>
+                    <div className="header-info">
+                      <h3 className="tagline">{movie.tagline}</h3>
+                      <h3 className="synopsis">Sinopse</h3>
+                      <div className="overview">
+                        <p>{movie.overview}</p>
+                      </div>
+                      <ol className="enterprises">
+                        <li className="profile">
+                          {movie.production_companies.map((company, index) => {
+                            return (
+                              <p>
+                                <img key={index} src={`https://image.tmdb.org/t/p/w500${company.logo_path}`} alt={company.name}/>
+                              </p>
+                            )
+                          })}
+                        </li>
+                      </ol>
                     </div>
                   </div>
                 </section>
@@ -47,6 +63,7 @@ export default function Post({ movie }) {
 }
 
 export async function getServerSideProps(context) {
+
   const { params } = context
 
   const key = "89088c5c3d55c787cf6ce7a00dfc52a0"
@@ -57,7 +74,34 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      movie,
+      movie 
     },
   }
 }
+
+// Elenco Principal:
+//https://api.themoviedb.org/3/movie/436270/credits?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=pt-BR
+
+// Palavras-chave:
+// https://api.themoviedb.org/3/movie/436270/keywords?api_key=89088c5c3d55c787cf6ce7a00dfc52a0
+
+// Recomendações:
+// https://api.themoviedb.org/3/movie/436270/recommendations?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=pt-BR&page=1
+
+// Reviews: 
+// https://api.themoviedb.org/3/movie/436270/reviews?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=pt-BR&page=1
+
+// Filmes similares:
+// https://api.themoviedb.org/3/movie/436270/similar?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=pt-BR&page=1
+
+// Vídeos:
+// https://api.themoviedb.org/3/movie/436270/videos?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=pt-BR
+
+// Mais Populares:
+// https://api.themoviedb.org/3/movie/top_rated?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=pt-BR&page=1
+
+// Próximos Lançamentos:
+// https://api.themoviedb.org/3/movie/upcoming?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=pt-BR&page=1
+
+// Biografia dos atores: 
+// https://api.themoviedb.org/3/person/18918?api_key=89088c5c3d55c787cf6ce7a00dfc52a0&language=en-US
--- pages/details/style.jsx
+++ pages/details/style.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components"
 
 export const StyledDetails = styled.div`
-
   .custom-bg {
     background-size: cover;
     background-repeat: no-repeat;
@@ -14,10 +13,19 @@ export const StyledDetails = styled.div`
   }
 
   .bg {
-    background-image: linear-gradient(to right,rgb(24 20 20) 150px, rgb(0 0 0 / 84%) 100%);
+    background-image: linear-gradient(
+      to right,
+      rgb(24 20 20) 150px,
+      rgb(0 0 0 / 84%) 100%
+    )
   }
 
   .infos {
+    display: flex;
+    justify-content: center;
+  }
+
+  .content {
     display: flex;
     justify-content: center;
   }
@@ -27,24 +35,75 @@ export const StyledDetails = styled.div`
     height: 500px;
   }
 
+  .posters-infos {
+    width: 60%;
+  }
+
+  .titles {
+    padding: 4rem;
+  }
+
   .title {
-    margin-bottom: .5rem;
+    font-size: 2.2rem;
   }
 
-  .content {
-    display: flex;
+  .title,
+  .facts,
+  .tagline {
+    margin-bottom: 0.5rem;
   }
 
-  .facts, .facts-infos {
+  .facts,
+  .facts-infos {
     display: flex;
-    
+    align-items: center;
   }
 
   .facts-infos {
     padding: 0 1rem 0 1rem;
 
     span {
-      padding: .5rem;
+      padding: 0.5rem;
+      font-size: 1.1rem;
+    }
+  }
+
+  .hour {
+    letter-spacing: 3px;
+  }
+
+  .tagline {
+    font-size: 1.1em;
+    font-weight: 400;
+    font-style: italic;
+    opacity: 0.7;
+  }
+
+  .synopsis {
+    font-size: 1.5rem;
+    margin: 1rem 0 1rem 0;
+  }
+
+  .overview {
+    p {
+      line-height: 1.5;
+    }
+  }
+
+  .enterprises {
+    list-style-type: none;
+    margin-top: 2rem;
+
+    li {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      align-items: center;
+      gap: 2rem;
+    }
+
+    img {
+      max-width: 1000px;
+      width: 25%;
     }
   }
 `
